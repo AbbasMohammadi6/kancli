e7e8d1d6970d3e76d828ad0ed9c6f50a6074069e
Abbas <abbasmohammadi6@gmail.com>
2025-01-30 23:09:31 +0330

    add columns and styles

---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+const divisor = 4
+
+var (
+	columnStyle = lipgloss.NewStyle().
+			Padding(1, 2)
+
+	focusedStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("62"))
+
+	helpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241"))
+)
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+type Model struct {
+	focused  status
+	lists    []list.Model
+	err      error
+	loaded   bool
+	quitting bool // to prevent the last render when after the user wants to quit
+}
+
+func (m *Model) initLists(width, height int) {
+	// there is a function called GetFrameSize that you can use to get the sum of the margins, padding and border width for  both the horizontal and vertical margins
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height-divisor)
+	defaultList.SetShowHelp(false)
+	m.lists = []list.Model{defaultList, defaultList, defaultList}
+
+	m.lists[todo].Title = "To Do"
+	m.lists[todo].SetItems([]list.Item{
+		Task{status: todo, title: "buy milk", description: "strawberry milk"},
+		Task{status: todo, title: "eat sushi", description: "rice"},
+		Task{status: todo, title: "fold laundery", description: "or wear wrinkly t-shirts"},
+	})
+
+	m.lists[inProgress].Title = "In Progress"
+	m.lists[inProgress].SetItems([]list.Item{
+		Task{status: inProgress, title: "write code", description: "don't worry its Go"},
+	})
+
+	m.lists[done].Title = "Done"
+	m.lists[done].SetItems([]list.Item{
+		Task{status: todo, title: "stay cool", description: "as a cucumber"},
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		if !m.loaded {
+			columnStyle.Width(msg.Width / divisor)
+			focusedStyle.Width(msg.Width / divisor)
+
+			columnStyle.Height(msg.Height - divisor)
+			focusedStyle.Height(msg.Height - divisor)
+
+			m.initLists(msg.Width, msg.Height)
+			m.loaded = true
+		}
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+
+		case "left", "h":
+			m.Previous()
+
+		case "right", "l":
+			m.Next()
+		}
+	}
+
+	var cmd tea.Cmd
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	if m.quitting {
+		return ""
+	}
+
+	if m.loaded {
+		todoView := m.lists[todo].View()
+		inProgressView := m.lists[inProgress].View()
+		doneView := m.lists[done].View()
+
+		switch m.focused {
+		case inProgress:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				columnStyle.Render(todoView),
+				focusedStyle.Render(inProgressView),
+				columnStyle.Render(doneView),
+			)
+
+		case done:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				columnStyle.Render(todoView),
+				columnStyle.Render(inProgressView),
+				focusedStyle.Render(doneView),
+			)
+
+		default:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				focusedStyle.Render(todoView),
+				columnStyle.Render(inProgressView),
+				columnStyle.Render(doneView),
+			)
+		}
+
+	} else {
+		return "loading..."
+	}
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func (m *Model) Next() {
+	if m.focused == done {
+		m.focused = todo
+	} else {
+		m.focused++
+	}
+}
+
+func (m *Model) Previous() {
+	if m.focused == todo {
+		m.focused = done
+	} else {
+		m.focused--
+	}
+}
+
+func main() {
+	program := tea.NewProgram(New())
+	_, err := program.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
